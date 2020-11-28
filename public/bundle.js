ed54a81de7fd2a7d409aa147f0006420ced25e6c
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-11-28 19:57:08 +0100

    setting up the frontend

--- public/bundle.js
+++ public/bundle.js
@@ -43,9 +43,39 @@ const contractABI =  [
 ];
 const contractAddress = '0x3DFDE7Df8443D12C7294F856a839bF15D87c4d58';
 const web3 = new Web3('http://localhost:9545');
-const SimpleStorage = new web3.eth.Contract(contractABI, contractAddress);
+const simpleStorage = new web3.eth.Contract(contractABI, contractAddress);
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+  const $setData = document.getElementById('setData');
+  const $data = document.getElementById('data');
+  let accounts = [];
+
+  web3.eth.getAccounts()
+    .then(_accounts => {
+      accounts = _accounts;
+    });
+
+  const getData = () => {
+    simpleStorage.methods
+      .get()
+      .call()
+      .then(result => {
+        $data.innerHTML = result;
+      })
+  };
+  getData();
+
+  $setData.addEventListener('submit', event => {
+    event.preventDefault();
+   
+    const data = event.target.elements[0].value;
+    console.log('value', data);
+    simpleStorage.methods
+      .set(data)
+      .send({ from: accounts[0] })
+      .then(getData);
+  });
 });
+
+
